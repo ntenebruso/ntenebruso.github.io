6d1a1864e34bd2b62110f2f7be317ac7aabdd84c
Nick Tenebruso <nbotminecraft@gmail.com>
2022-10-16 21:21:02 -0400

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            sans: ["Inter", "sans-serif"],
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
