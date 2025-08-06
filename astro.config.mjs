e412325d833c4576477eaf6d55c7d222f8d1af31
Nick Tenebruso <nickten2006@gmail.com>
2025-08-06 14:28:35 -0400

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,20 +8,9 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
-// swup
-import swup from "@swup/astro";
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        mdx(),
-        svelte(),
-        swup({
-            accessibility: true,
-            theme: false,
-        }),
-    ],
+    integrations: [tailwind(), mdx(), svelte()],
     markdown: {
         syntaxHighlight: "prism",
     },
